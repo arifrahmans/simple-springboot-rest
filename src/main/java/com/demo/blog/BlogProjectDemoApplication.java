a173382620ab0ed28db4ffbbfb583c422432d778
arifrahmans <arif.ilkom@gmail.com>
2018-07-05 12:49:57 +0700

    initial commit

---
+++ src/main/java/com/demo/blog/BlogProjectDemoApplication.java
@@ -0,0 +1,27 @@
+package com.demo.blog;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses= {
+		BlogProjectDemoApplication.class,
+		Jsr310Converters.class
+})
+public class BlogProjectDemoApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjectDemoApplication.class, args);
+	}
+}
